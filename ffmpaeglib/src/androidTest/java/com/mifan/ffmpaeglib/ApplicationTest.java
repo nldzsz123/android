adcf4c29edd39bd208dc3cbbeed00dd51d0edc1e
nldzsz <394360192@qq.com>
2022-06-08 23:31:49 +0800

    开始

--- ffmpaeglib/src/androidTest/java/com/mifan/ffmpaeglib/ApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.mifan.ffmpaeglib;
-
-import android.app.Application;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest {
-}
