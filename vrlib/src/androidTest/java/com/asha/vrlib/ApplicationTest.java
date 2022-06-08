870405f1c627bead700f827d5647b00eda8b0ae2
nldzsz <394360192@qq.com>
2022-06-08 23:34:45 +0800

    开始

--- vrlib/src/androidTest/java/com/asha/vrlib/ApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.asha.vrlib;
-
-import android.app.Application;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest {
-}
