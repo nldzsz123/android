ac655ac9bab12909e71ce0c160c495cb7ceb3e4f
nldzsz <394360192@qq.com>
2022-06-08 23:45:40 +0800

    开始

--- kprogresshud/src/androidTest/java/com/kaopiz/kprogresshud/ApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.kaopiz.kprogresshud;
-
-import android.app.Application;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest {
-}
